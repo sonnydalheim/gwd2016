d4febb38eadf55d04d6da5da94bd63ce9bd67536
sonnydalheim <sonny_dalheim@hotmail.com>
2016-05-12 17:46:27 +0200

    Subtle sound when the ball is rolling + attempt at making god ray for magnifying glass (currently turned off).

---
+++ Assets/_Game/Scripts/ballController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballController : MonoBehaviour {
+
+	public AudioClip rollSound;
+	AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (GetComponent<Rigidbody>().velocity.x > 0) {
+			if(!audio.isPlaying) {
+				audio.PlayOneShot(rollSound, 0.1f);
+			}
+		}
+		else
+		{
+			GetComponent<AudioSource>().Stop();
+		}
+	
+	}
+}
